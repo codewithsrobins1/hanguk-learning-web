e39d2cc6e0eb1e81f4df036cbe933388de6812c3
codewithsrobins1 <codewithsrobins1@gmail.com>
2026-03-07 18:17:53 -0800

    chore: fix pwa files

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,5 @@
 import type { NextConfig } from 'next';
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development', // only active in production
-  register: true,
-  skipWaiting: true,
-});
 
 const nextConfig: NextConfig = {};
 
-module.exports = withPWA(nextConfig);
+export default nextConfig;
